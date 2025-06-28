b80212649643c67f97ac149277b7f98accda05ac
nenini <kyr068600@gmail.com>
2025-06-28 18:36:24 +0900

    feat: Add day4-project

---
+++ day4-project/src/main/java/com/example/day4_project/Day4ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.day4_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4ProjectApplication.class, args);
+	}
+
+}
