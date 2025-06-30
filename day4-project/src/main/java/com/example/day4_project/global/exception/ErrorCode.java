09eb930354d49f173d429b99d2bfedfcfa192834
nenini <kyr068600@gmail.com>
2025-07-01 02:18:14 +0900

    style : ErrorCode.java 변수명 변경

--- day4-project/src/main/java/com/example/day4_project/global/exception/ErrorCode.java
+++ day4-project/src/main/java/com/example/day4_project/global/exception/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND,"POST-001","게시글을 찾을 수 없습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,"USER-001","사용자를 찾을 수 없습니다."),
-    VLIDATION_ERROR(HttpStatus.BAD_REQUEST,"COMMON-001","잘못된 요청입니다."),
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST,"COMMON-001","잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COMMON-999","서버 오류입니다.");
     private final HttpStatus status;
     private final String code;
